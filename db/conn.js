31359a65c213fcfef680a3835689a714790e8cd5
Antonio Alves <aaba03@gmail.com>
2023-06-10 23:38:05 -0300

    deploy part 3

--- db/conn.js
+++ db/conn.js
@@ -1,21 +1,17 @@
 const mysql = require('mysql2/promise');
 const Sequelize = require('sequelize');
 
-async function createDatabase() {
-  const connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: ''
-  });
 
-  await connection.query('CREATE DATABASE IF NOT EXISTS diarioyanna');
+async function createDatabase() {
+  const connection = await mysql.createConnection(
+    'mysql://uigd2pxdqjqaegcc:DHJWSfmX7P8wXSAu2kp0@btkssictpreeajudgtae-mysql.services.clever-cloud.com:3306/btkssictpreeajudgtae'
+  );
+
+  //await connection.query('CREATE DATABASE IF NOT EXISTS diarioyanna');
   connection.end();
 }
 
-const sequelize = new Sequelize('diarioyanna', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
+const sequelize = new Sequelize('mysql://uigd2pxdqjqaegcc:DHJWSfmX7P8wXSAu2kp0@btkssictpreeajudgtae-mysql.services.clever-cloud.com:3306/btkssictpreeajudgtae');
 
 module.exports = sequelize;
 
